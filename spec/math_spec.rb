5a24bc5390ed844c2e1687dbc1630bd20cefa5ba
Ben Snape <ben.snape@itv.com>
2014-09-12 12:40:06 +0100

    added two failing tests for Sensu to report on

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -4,4 +4,12 @@ describe 'Simple Mathematical Operations' do
     expect(1+1).to eq 2
   end
 
+  it 'example failure one' do
+    expect(1).to eq 2
+  end
+
+  it 'example failure two' do
+    expect(2).to eq 1
+  end
+
 end
