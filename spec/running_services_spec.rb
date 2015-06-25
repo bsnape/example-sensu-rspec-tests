50fd1f1c546802c39c56d71328df5c80c7e81e46
Ben Snape <ben.snape@itv.com>
2015-06-25 16:35:41 +0100

    removed sudo command and using ps instead

--- spec/running_services_spec.rb
+++ spec/running_services_spec.rb
@@ -1,18 +1,16 @@
 describe 'Critical Services' do
 
-  it 'is running the SSH daemon' do
-    status        = `sudo service sshd status`
-    error_message = "The sshd service is not running! \n\n Got the following output when querying the status: '#{status}'"\
-                    "\n Try starting the service with: 'sudo service sshd start'."
-    expect(status).to_not include('stopped'), error_message
+  it 'SSH daemon' do
+    status        = `ps aux | grep [s]shd`
+    error_message = "The sshd service is not running! \n\n Try starting the service with: 'sudo service sshd start'."
+    expect(status).to_not be_empty, error_message
   end
 
-  it 'is running the nginx daemon' do
-    status        = `sudo service nginx status`
-    error_message = "The nginx service is not running! \n\n Got the following output when querying the status: '#{status}'"\
-                    "\n Try starting the service with: 'sudo service nginx start'."\
+  it 'nginx daemon' do
+    status        = `ps aux | grep [n]ginx`
+    error_message = "The nginx service is not running! \n\n Try starting the service with: 'sudo service nginx start'."\
                     "\n\n To investigate further, inspect the logs at /var/log/nginx/{access,error}.log"
-    expect(status).to_not include('stopped'), error_message
+    expect(status).to_not be_empty, error_message
   end
 
 end
